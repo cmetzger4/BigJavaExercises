c6f98490d30aed5f6b81ecdded1f3a36135a6c2d
Charles Metzger <cmetzger4@gmail.com>
2015-04-16 15:12:50 -0400

    Created FacePrinter.java as a solution to Exercise P1.3 (the face this program creates is different from the example within the problem description).
    Updated Exercises README.txt with problem description for P1.3.

--- src/Chapter01/Exercises README.txt
+++ src/Chapter01/Exercises README.txt
@@ -17,3 +17,17 @@ Write a program that prints your name in large letters, such as:
     *   *  ******  *   *   *   *    *
     *   *  *    *  *    *  *    *   *
 
+
+P1.3
+Write a program FacePrinter that prints a face, using text character, hopefully better looking than this one:
+
+       //////
+      | 0 0 |
+     (|  ^  |)
+      | [_] |
+       -----
+Use comments to indicate the statements that print the hair, ears, mouth, and so on.
+
+  **My notes on exercise P1.3:
+    I used some backslashes in my face construction and each one needs to be prefaced with an additional backslash as
+    an escape character.
---
+++ src/Chapter01/FacePrinter.java
@@ -0,0 +1,33 @@
+package Chapter01;
+
+/**
+ *  Application: BlockLetterPrinter
+ *  Created on:  2015.04.16
+ *
+ *  GitHub Repo: cmetzger4/BigJavaExercises
+ */
+
+public class FacePrinter {
+
+    public static void main(String[] args) {
+
+        // This section is the face's hairline and forehead
+        System.out.println("        \\\\\\\\\\\\///////");
+        System.out.println("        |           |");
+
+        //This section is the face's eyes/glasses
+        System.out.println("       /---\\____/---\\");
+        System.out.println("       | O |/  \\| O | ");
+        System.out.println("       \\---/    \\---/");
+
+        //This section is the face's nose area
+        System.out.println("      /   { ~  ~ }   \\");
+
+        //This section is the mouth and chin area of the face
+        System.out.println("     |  _         _   |");
+        System.out.println("     \\   \\_______/    /");
+        System.out.println("      \\              /");
+        System.out.println("       \\___(_^^_)___/");
+
+    }
+}
