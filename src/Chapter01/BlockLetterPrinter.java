f61cf57662b84f44223a9e6ae9ec21df65c4dc86
Charles Metzger <cmetzger4@gmail.com>
2015-04-16 10:17:05 -0400

    Created BlockLetterPrinter.java as a solution to Exercise P1.2.  Currently, "CHARLES" is hard-coded for display. Updates Exercises README.txt with problem description for P1.2.

---
+++ src/Chapter01/BlockLetterPrinter.java
@@ -0,0 +1,21 @@
+package Chapter01;
+
+/**
+ *  Application: BlockLetterPrinter
+ *  Created on:  2015.04.16
+ *
+ *  GitHub Repo: cmetzger4/BigJavaExercises
+ */
+
+public class BlockLetterPrinter {
+
+    public static void main(String[] args) {
+
+        System.out.println("   ***   *   *    **    ****    *      *****  *****");
+        System.out.println("  *   *  *   *   *  *   *   *   *      *      *");
+        System.out.println(" *       *****  *    *  ****    *      ***     ***");
+        System.out.println("  *   *  *   *  ******  *   *   *      *          *");
+        System.out.println("   ***   *   *  *    *  *    *  *****  *****  *****");
+
+    }
+}
--- src/Chapter01/Exercises README.txt
+++ src/Chapter01/Exercises README.txt
@@ -2,7 +2,18 @@ Chapter 1 Exercises
 
 P1.1
 Write a program NamePrinter that displays your name inside a box on the console screen, like this:
+
     +----+
     |Dave|
     +----+
+
+
+P1.2
+Write a program that prints your name in large letters, such as:
+
+    *   *    **    ****    ****   *   *
+    *   *   *  *   *   *   *   *  *   *
+    *****  *    *  ****    ****    * *
+    *   *  ******  *   *   *   *    *
+    *   *  *    *  *    *  *    *   *
 
