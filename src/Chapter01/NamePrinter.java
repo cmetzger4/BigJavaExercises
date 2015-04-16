c51a96e36483fb4d65111326d8e2a64465e447ad
Charles Metzger <cmetzger4@gmail.com>
2015-04-16 09:44:02 -0400

    Created NamePrinter.java as simple solution to problem P1.1.  It has the string literals hardcoded.

---
+++ src/Chapter01/NamePrinter.java
@@ -0,0 +1,19 @@
+package Chapter01;
+
+/**
+ *  Application: NamePrinter
+ *  Created on:  2015.04.16
+ *
+ *
+ */
+
+public class NamePrinter {
+
+    public static void main(String[] args) {
+
+        System.out.println("+-------+");
+        System.out.println("|Charles|");
+        System.out.println("+-------+");
+
+    }
+}
